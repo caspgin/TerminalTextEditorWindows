db8dff93e26f02088eaa9be34a096cb214bfe7d2
caspgin <31950557+caspgin@users.noreply.github.com>
2025-07-08 11:39:56 -0700

    Enabled RawMode but still Processing CTRL+C and perhaps more

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,39 @@
 
 #include <Windows.h>
+#include <consoleapi.h>
 #include <errhandlingapi.h>
 #include <handleapi.h>
 #include <minwindef.h>
 #include <winbase.h>
 
+#include <cstdlib>
 #include <iostream>
 
+DWORD originalMode;
+HANDLE hInput;
+
+void disableRawMode() { SetConsoleMode(hInput, originalMode); }
+
+void enableRawMode() {
+    // Turn off echo
+    GetConsoleMode(hInput, &originalMode);
+    std::atexit(disableRawMode);
+
+    DWORD rawMode = originalMode;
+    rawMode &= ~ENABLE_ECHO_INPUT;
+    rawMode &= ~ENABLE_LINE_INPUT;
+    SetConsoleMode(hInput, rawMode);
+}
+
 int main() {
-    HANDLE hOutput = GetStdHandle(STD_OUTPUT_HANDLE);
-    HANDLE hInput = GetStdHandle(STD_INPUT_HANDLE);
+    hInput = GetStdHandle(STD_INPUT_HANDLE);
     if (hInput == INVALID_HANDLE_VALUE) {
         std::cerr << "Error: Could not get standard input handle. Code: "
                   << GetLastError() << std::endl;
         return 1;
     }
+
+    enableRawMode();
 
     char c;
     DWORD bytesRead;
