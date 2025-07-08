a5b8402218466dfd4ffe838828fc1bf61e7e7a4d
caspgin <31950557+caspgin@users.noreply.github.com>
2025-07-08 01:04:16 -0700

    Reading Input Buffer using ReadFile 1 byte at a time in cooked mode and quitting at eof or when q is processed

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,23 @@
 
 #include <Windows.h>
+#include <errhandlingapi.h>
+#include <handleapi.h>
+#include <minwindef.h>
 #include <winbase.h>
 
-int main() {
-    HANDLE OutputH = GetStdHandle(STD_OUTPUT_HANDLE);
-    HANDLE InputH = GetStdHandle(STD_INPUT_HANDLE);
+#include <iostream>
 
+int main() {
+    HANDLE hOutput = GetStdHandle(STD_OUTPUT_HANDLE);
+    HANDLE hInput = GetStdHandle(STD_INPUT_HANDLE);
+    if (hInput == INVALID_HANDLE_VALUE) {
+        std::cerr << "Error: Could not get standard input handle. Code: "
+                  << GetLastError() << std::endl;
+        return 1;
+    }
+
+    char c;
+    DWORD bytesRead;
+    while (ReadFile(hInput, &c, sizeof(c), &bytesRead, NULL) && c != 'q');
     return 0;
 }
